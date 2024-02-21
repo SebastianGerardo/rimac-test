ff243c09306085c46ad636c7f3651731e1b10648
Sebastián Gerardo <sebastiangerardo27@gmail.com>
2024-02-21 14:04:57 -0500

    refactoring code 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom'
+// pages
 const Home = lazy(() => import('@/pages/Home/Home'));
 const PaymentDetail = lazy(() => import('@/pages/PaymentDetail/PaymentDetail'));
 const PlansSection = lazy(() => import('@/pages/PlansSection/PlansSection'));
